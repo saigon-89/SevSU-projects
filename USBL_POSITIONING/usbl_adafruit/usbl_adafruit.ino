9ce2968da159e6b9f62f0d37718fc4b91abef826
saigon-89 <mash.saigon.89@gmail.com>
2023-09-19 18:09:00 +0300

    Update usbl_adafruit.ino

--- USBL_POSITIONING/usbl_adafruit/usbl_adafruit.ino
+++ USBL_POSITIONING/usbl_adafruit/usbl_adafruit.ino
@@ -1,9 +1,11 @@
 #include <Adafruit_MPU6050.h>
+#include <Adafruit_HMC5883_U.h>
 #include <Adafruit_Sensor.h>
 #include <MadgwickAHRS.h>
 #include <Wire.h>
 
 Adafruit_MPU6050 mpu;
+Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(123);
 Madgwick filter;
 unsigned long microsPerReading, microsPrevious;
 
@@ -13,7 +15,7 @@ void setup(void) {
     delay(10);
   }
 
-  filter.begin(10); // TODO
+  filter.begin(10);
 
   if (!mpu.begin()) {
     Serial.println("Failed to find MPU6050 chip");
@@ -21,9 +23,12 @@ void setup(void) {
       delay(10);
     }
   }
-
   mpu.setAccelerometerRange(MPU6050_RANGE_2_G);
   mpu.setGyroRange(MPU6050_RANGE_250_DEG);
+
+  if (!mag.begin()) {
+    Serial.println("Failed to find HMC5883 chip");
+  }
 
   // initialize variables to pace updates to correct rate
   microsPerReading = 10000;
@@ -36,14 +41,15 @@ void setup(void) {
 void loop() {
   unsigned long microsNow;
   float phi, theta, psi;
-  float ax, ay, az, gx, gy, gz;
+  float ax, ay, az, gx, gy, gz, mx, my, mz;
 
   // check if it's time to read data and update the filter
   microsNow = micros();
   if (microsNow - microsPrevious >= microsPerReading) {
     /* Get new sensor events with the readings */
-    sensors_event_t a, g, temp;
+    sensors_event_t a, g, m, temp;
     mpu.getEvent(&a, &g, &temp);
+    mag.getEvent(&m);
 
     gx = g.gyro.x;
     gy = g.gyro.y;
@@ -51,9 +57,12 @@ void loop() {
     ax = a.acceleration.x;
     ay = a.acceleration.y;
     az = a.acceleration.z;
+    mx = m.magnetic.x;
+    my = m.magnetic.y;
+    mz = m.magnetic.z;
 
     // update the filter, which computes orientation
-    filter.updateIMU(gx, gy, gz, ax, ay, az);
+    filter.update(gx, gy, gz, ax, ay, az, mx, my, mz);
     
     phi = filter.getRoll();
     theta = filter.getPitch();
@@ -72,7 +81,5 @@ void loop() {
 
     // increment previous time, so we keep proper pace
     microsPrevious = microsPrevious + microsPerReading;
-    
-    //delay(10);
   }
 }
