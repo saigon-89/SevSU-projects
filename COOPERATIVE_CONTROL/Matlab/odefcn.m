d488d980daacca69969d57a7dd309678b5928aaf
saigon-89 <mash.saigon.89@gmail.com>
2023-11-10 17:07:14 +0300

    Добавлена поддержка работы с группой ПА

--- COOPERATIVE_CONTROL/Matlab/odefcn.m
+++ COOPERATIVE_CONTROL/Matlab/odefcn.m
@@ -1,20 +1,30 @@
-function dx = odefcn(t,x,UV,tau,he)
-    if nargin < 5
-        he = zeros(6,1);
-    end
+function dx = odefcn(t,x,UV,TAU,HE)
     n = 6;
-    eta = x(1:n); 
-    v = x(7:end);
-
-    M = UV.M;
-    C = UV.C(UV,v);
-    g = UV.g(UV,eta);
-    J = UV.J(eta);
-
-    A = [zeros(n), J^-1;
-         zeros(n), -M^-1 * ( C )];
-    B = [zeros(n); M^-1];
-
-    u = -g + tau + J*he; % без управления
-    dx = A*x + B*u;
+    dx = zeros(2*n*numel(UV),1);
+    for i = 1:numel(UV)
+        offset = 12 * (i - 1);
+        eta = x((1 + offset):(n + offset));
+        v = x((7 + offset):(2*n + offset));
+    
+        M = UV{i}.M;
+        %C = UV{i}.C(UV{i},v);
+        C = zeros(6,6);
+        g = UV{i}.g(UV{i},eta);
+        J = UV{i}.J(eta);
+    
+        A = [zeros(n), J^-1;
+             zeros(n), -M^-1 * ( C )];
+        B = [zeros(n); M^-1];
+    
+        if nargin < 5
+            he = zeros(n,1);
+        else
+            he = HE{i};
+        end
+        
+        tau = TAU{i};
+        u = -g + tau + J*he; % без управления
+        offset = 12 * (i - 1);
+        dx((1 + offset):(2*n + offset)) = A*x((1 + offset):(2*n + offset)) + B*u;
+    end
 end
