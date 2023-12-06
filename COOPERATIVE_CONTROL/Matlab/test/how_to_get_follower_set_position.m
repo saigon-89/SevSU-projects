dc6c0d3c939a92e4ebe2c5cd21f7f2936a9d0480
saigon-89 <mash.saigon.89@gmail.com>
2023-12-06 14:23:29 +0300

    Добавлены примеры расчета уставок для ведомых ПА

---
+++ COOPERATIVE_CONTROL/Matlab/test/how_to_get_follower_set_position.m
@@ -0,0 +1,70 @@
+clc
+close all
+clear all
+pos_l = [1; 0]; % Leader Position
+pos_f = [5; 1]; % Follower Position
+r = 2; % Range
+
+%% GROUP 1: IF LEADER AND FOLLOWER POSITIONS ARE KNOWN
+%% VIA NUMERICAL OPTIMIZATION
+tic
+f = @(x)Criterion(x, pos_l, r);
+constr = @(x)Constrains(x, pos_l, r);
+[x, fval] = fmincon(f, pos_f, [], [], [], [], [], [], constr);
+toc
+disp('Numerical optimization');
+
+figure
+plot(pos_l(1), pos_l(2), 'rO', 'LineWidth', 3);
+viscircles(pos_l', r);
+hold on
+plot(pos_f(1), pos_f(2), 'bO', 'LineWidth', 2);
+plot(x(1), x(2), 'gO', 'LineWidth', 2);
+legend('Leader Position', 'Follower Position', 'Follower Set Position')
+grid on
+axis equal
+xlabel('X, m')
+ylabel('Y, m')
+
+%% ANALITICALLY
+tic
+P1 = @(pos_l, pos_f, r) [ pos_l(1) + (r*(pos_f(1)-pos_l(1))) / sqrt((pos_f(1)-pos_l(1))^2 + (pos_f(2)-pos_l(2))^2); ...
+                          pos_l(2) + (r*(pos_f(2)-pos_l(2))) / sqrt((pos_f(1)-pos_l(1))^2 + (pos_f(2)-pos_l(2))^2) ];
+P2 = @(pos_l, pos_f, r) [ pos_l(1) - (r*(pos_f(1)-pos_l(1))) / sqrt((pos_f(1)-pos_l(1))^2 + (pos_f(2)-pos_l(2))^2); ...
+                          pos_l(2) - (r*(pos_f(2)-pos_l(2))) / sqrt((pos_f(1)-pos_l(1))^2 + (pos_f(2)-pos_l(2))^2) ];
+% Две возможные точки
+p1 = P1(pos_l, pos_f, r);
+p2 = P2(pos_l, pos_f, r);
+% Две нормы
+n = [ norm(p1 - pos_f); ... 
+      norm(p2 - pos_f) ];
+% Ищем минимальную
+[~, idx] = min(n);
+if (idx == 1)
+    x(1) = p1(1); x(2) = p1(2);
+else
+    x(1) = p2(1); x(2) = p2(1);
+end
+toc
+disp('Analitically');
+
+figure
+plot(pos_l(1), pos_l(2), 'rO', 'LineWidth', 3);
+viscircles(pos_l', r);
+hold on
+plot(pos_f(1), pos_f(2), 'bO', 'LineWidth', 2);
+plot(x(1), x(2), 'gO', 'LineWidth', 2);
+legend('Leader Position', 'Follower Position', 'Follower Set Position')
+grid on
+axis equal
+xlabel('X, m')
+ylabel('Y, m')
+
+function f = Criterion(pos_f, pos_l, r)
+    f = sqrt( (pos_f(1) - pos_l(1))^2 + (pos_f(2) - pos_l(2))^2 ) - r;
+end
+
+function [c, ceq] = Constrains(pos_f, pos_l, r)
+    c = -( (pos_f(1) - pos_l(1))^2 + (pos_f(2) - pos_l(2))^2 - r^2);
+    ceq = [];
+end
