d8c40aa3c030ea8ab8b75cdaffc3304aff5e727d
monsterkakes <mosnterkakes@gmail.com>
2025-02-16 14:38:23 +0300

    add simulation with multiple agents

--- Multiagent Reinforcement Learning/Trajectories Generation/python/simulate_vector.py
+++ Multiagent Reinforcement Learning/Trajectories Generation/python/simulate_vector.py
@@ -39,51 +39,61 @@ DISTANCE_STATE_SIZE = int(config.get("DISTANCE_STATE_SIZE"))
 OBSTACLE_DISTANCE_STATE_SIZE = int(config.get("OBSTACLE_DISTANCE_STATE_SIZE"))
 CLOSE_OBSTACLES_STATE_SIZE = int(config.get("CLOSE_OBSTACLES_STATE_SIZE"))
 
-for iteration in range(100):
-    # agent_pos = [2, 10]
-    # goal_pos = [23, 15]
-    agent_pos = [random.randint(0, MAP_SIZE-1), random.randint(0, MAP_SIZE-1)]
-    goal_pos = [random.randint(0, MAP_SIZE-1), random.randint(0, MAP_SIZE-1)]
-    obstacles = []
+AGENTS_AMOUNT = 3
+OBSTACLES_AMOUNT = 15
 
-    agent = Point(*agent_pos)
-    goal = Point(*goal_pos)
+for iteration in range(100):
+    agents = []
+    goals = []
+    obstacles = []
+    while (len(agents) < AGENTS_AMOUNT):
+        p = Point(random.randint(0, MAP_SIZE-1), random.randint(0, MAP_SIZE-1))
+        if p not in agents and p not in goals:
+            agents.append(p)
+    while (len(goals) < AGENTS_AMOUNT):
+        p = Point(random.randint(0, MAP_SIZE-1), random.randint(0, MAP_SIZE-1))
+        if p not in agents and p not in goals:
+            goals.append(p)
+    while (len(obstacles) < OBSTACLES_AMOUNT):
+        p = Point(random.randint(0, MAP_SIZE-1), random.randint(0, MAP_SIZE-1))
+        if p not in agents and p not in goals:
+            obstacles.append(p)
 
     max_possible_distance = math.ceil(math.sqrt(2) * MAP_SIZE)
 
     map = np.zeros((MAP_SIZE, MAP_SIZE))
-    map[agent.x, agent.y] = MAP_START_POS
-
-    for i in range(15):
-        obs = Point(random.randint(0, MAP_SIZE-1), random.randint(0, MAP_SIZE-1))
-        if obs != goal and obs != agent:
-            obstacles.append(obs)
-
+    for ag in agents:
+        map[ag.x, ag.y] = MAP_START_POS
     for o in obstacles:
         map[o.x, o.y] = MAP_OBSTACLE
 
     try:
         for i in range(50):
-            distance = calc_distance(agent, goal)
-            angle = calc_angle(agent, goal)
-            distance_state = distance_to_state(distance, max_possible_distance, DISTANCE_STATE_SIZE)
-            angle_state = angle_to_state(angle, ANGLE_STATE_SIZE)
-            obstacles_state, dto_state = calc_obstacles_state(agent, goal, obstacles, max_possible_distance, OBSTACLE_DISTANCE_STATE_SIZE)
-            close_obstacles_state = calc_close_obstacles_state(agent, goal, obstacles)
-            state = Q_table[distance_state, angle_state, obstacles_state, dto_state, close_obstacles_state]
-            action_index = np.argmax(state)
-            action = actions[action_index]
-            action(point=agent)
-            if agent in obstacles:
-                map[agent.x, agent.y] = MAP_COLLISION
-            else:
-                map[agent.x, agent.y] = MAP_PATH
-            if calc_distance(agent, goal) == 0:
-                break
+            for agent, goal in zip(agents, goals):
+                if calc_distance(agent, goal) == 0:
+                    continue
+                agents_and_obstacles = obstacles + agents
+                agents_and_obstacles.remove(agent)
+                distance = calc_distance(agent, goal)
+                angle = calc_angle(agent, goal)
+                distance_state = distance_to_state(distance, max_possible_distance, DISTANCE_STATE_SIZE)
+                angle_state = angle_to_state(angle, ANGLE_STATE_SIZE)
+                obstacles_state, dto_state = calc_obstacles_state(agent, goal, agents_and_obstacles, max_possible_distance, OBSTACLE_DISTANCE_STATE_SIZE)
+                close_obstacles_state = calc_close_obstacles_state(agent, goal, agents_and_obstacles)
+                state = Q_table[distance_state, angle_state, obstacles_state, dto_state, close_obstacles_state]
+                action_index = np.argmax(state)
+                action = actions[action_index]
+                action(point=agent)
+                if agent in agents_and_obstacles:
+                    map[agent.x, agent.y] = MAP_COLLISION
+                else:
+                    map[agent.x, agent.y] = MAP_PATH
+
     except Exception as e:
         print(e)
 
-    map[goal.x, goal.y] = MAP_GOAL
+    for g in goals:
+        map[g.x, g.y] = MAP_GOAL
     plt.imshow(map, cmap=cmap)
     # plt.show()
     plt.savefig(f"results/{datetime.now().timestamp()}.png")
